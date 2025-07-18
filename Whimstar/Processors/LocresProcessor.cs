5bd722bd809826b98ef039749960573645851c8e
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2025-07-18 21:12:39 +0200

    make locres processor only finish after exporting all language codes

--- Whimstar/Processors/LocresProcessor.cs
+++ Whimstar/Processors/LocresProcessor.cs
@@ -1,3 +1,23 @@
-﻿namespace Whimstar.Processors;
+﻿using CUE4Parse.UE4.VirtualFileSystem;
 
-public class LocresProcessor(string output) : BaseFolderProcessor(Path.Join(output, "localization"), "X6Game/Content/Localization");
+namespace Whimstar.Processors;
+
+public class LocresProcessor(string output) : IArchiveProcessor
+{
+    private static readonly string[] LanguagesToExtract =
+        ["de", "en", "es", "fr", "id", "it", "ja-JP", "ko", "pt", "th", "zh", "zh-Hant", "zh-SG"];
+
+    private readonly LocresLanguageProcessor[] _languageProcessors =
+        LanguagesToExtract.Select(x => new LocresLanguageProcessor(output, x)).ToArray();
+
+    public bool Finished => _languageProcessors.All(x => x.Finished);
+
+    public async Task Process(IAesVfsReader archive, NikkiVersion version)
+    {
+        foreach (var processor in _languageProcessors.Where(x => !x.Finished))
+            await processor.Process(archive, version);
+    }
+
+    private class LocresLanguageProcessor(string output, string languageCode)
+        : BaseFolderProcessor(Path.Join(output, "localization", "Game", languageCode), $"X6Game/Content/Localization/Game/{languageCode}");
+}
